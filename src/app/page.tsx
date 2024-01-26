0fe91a7505ce5e5bc511878e93c16d837d255457
Sofia Bargues <barguessofia@gmail.com>
2024-01-26 14:06:44 +0000

    es por ahi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,23 @@ import { useFormState } from "react-dom";
 
 export default function HomePage() {
   const [state, formAction] = useFormState(createPrediction, null);
-  console.log(state);
 
   return (
-    <form action={formAction} className="m-auto grif max-w-[512px] gap-4">
-      <Input
-        defaultValue="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
-        name="image"
-        placeholder="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
-        type="text"
-      />
-      <Textarea placeholder="An industrial bedroom" name="prompt" />
-      <Button>Create</Button>
-    </form>
+    <section className="m-auto grid max-w-[512px] gap-4">
+      {state?.output && (
+        <img alt="Previsualizacion del render" src={state.output[1]} />
+      )}
+
+      <form action={formAction} className="grid gap-4">
+        <Input
+          defaultValue="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
+          name="image"
+          placeholder="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
+          type="text"
+        />
+        <Textarea placeholder="An industrial bedroom" name="prompt" />
+        <Button>Create</Button>
+      </form>
+    </section>
   );
 }
