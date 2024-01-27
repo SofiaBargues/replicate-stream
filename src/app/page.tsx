cfa9612d2a017956001dd955f282ec0c7f34729b
Sofia Bargues <barguessofia@gmail.com>
2024-01-27 08:54:17 +0000

    fin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 "use client";
+import { useFormState, useFormStatus } from "react-dom";
+
 import { createPrediction, getPrediction, runModel } from "@/actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Textarea } from "@/components/ui/textarea";
 import { Prediction } from "@/types";
-import { useFormState, useFormStatus } from "react-dom";
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+import { Label } from "@/components/ui/label";
 
 function FormContent() {
   const { pending } = useFormStatus();
+
   return (
     <>
       {pending ? <Skeleton className="h-[480px] w-[512px]" /> : null}
+      <Label htmlFor="image">Image</Label>
       <Input
         defaultValue="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
         name="image"
         placeholder="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
         type="file"
       />
-      <Textarea placeholder="An industrial bedroom" name="prompt" />
+      <Label htmlFor="prompt">What is in it</Label>
+      <Textarea name="prompt" placeholder="A puppy" />
       <Button disabled={pending}>Create</Button>
     </>
   );
@@ -34,7 +38,7 @@ export default function HomePage() {
 
   return (
     <section className="m-auto grid max-w-[512px] gap-4">
-      {state && <img alt="Previsualizacion del render" src={state[0]} />}
+      {state ? <img alt="Previsualizacion del render" src={state[0]} /> : null}
 
       <form action={formAction} className="grid gap-4">
         <FormContent />
